7ace6eb9acb1367eab5bba936842bd2c92ea4155
ErikdelRio <erik.delrio@ciencias.unam.mx>
2022-05-26 16:39:44 -0500

    Add: gaurda listas. Y obtiene el metadata para el front

--- FCMusic/sitio/views.py
+++ FCMusic/sitio/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Cancion,Lista_reproduccion
+from .models import Cancion,Lista_reproduccion,Cancion_lista_reproduccion
 import json
 
 # Create your views here.
+def parseRequest(req):
+    body_unicode = req.body
+    body = json.loads(body_unicode)
+    return body
+
+    
+def home(request):
+    listas = Lista_reproduccion \
+        .objects \
+        .filter(usuario_id=request.user.id)
+
+    return render(request, 'home.html', {'listas': listas})
+
 def escuchar(request):
     return render(request, 'cancion/escuchar.html')
 
@@ -21,20 +34,44 @@ def getCancion(request):
     return HttpResponse(json.dumps(canciones))
     # return render(request, json.dumps(canciones))
 
+def getCancionesLista(request):
+    body_unicode = request.body
+    body = json.loads(body_unicode)
+
+    user_id = request.user.id
+    lista_id = body['lista_id']
+    res = Cancion_lista_reproduccion.objects \
+        .filter(Lista_reproduccion=lista_id) \
+        .selectRelated('Cancion')
+
+    return list(res)
+    # return render(request, )
+
+def escucharLista(request):
+    lista = getCancionesLista(request)
+    # listas = Lista_reproduccion \
+    #    .objects \
+    #    .filter(usuario_id=request.user.id) \
+    #    .selectRelated('Cancion')
+
+    return render(request, 'cancion/escuchar.html', {'lista': lista})
+
 def creaLista(request):
     return render(request, 'cancion/creaLista.html')
 
-def home(request):
-    listas = Lista_reproduccion \
-        .objects \
-        .filter(usuario_id=request.user.id)
+def putLista(request):
+    body = parseRequest(request)
+    nuevaLista = Lista_reproduccion()
+    nuevaLista.usuario_id = request.user.id
+    nuevaLista.estado_publico = body['estado']
+    nuevaLista.nombre = body['nombre']
+    nuevaLista.save()
 
-    return render(request, 'home.html', {'listas': listas})
-
-def escucharLista(request):
-    listas = Lista_reproduccion \
-        .objects \
-        .filter(usuario_id=request.user.id) \
-        .selectRelated('Cancion')
-
-    return render(request, 'cancion/escuchar.html')
+    i = 0
+    for c in body['canciones']:
+        nuevaCancionLista = Cancion_lista_reproduccion()
+        nuevaCancionLista.cancion_id = c.cancion_id
+        nuevaCancionLista.indice = i
+        nuevaCancionLista.save()
+    
+    return HttpResponseRedirect('/home')
