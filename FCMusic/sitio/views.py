5780134d832723b7ceebea9964bf0f16289adfde
ErikdelRio <erik.delrio@ciencias.unam.mx>
2022-05-28 14:18:16 -0500

    Add: mensaje de errores 400, 403, 404, 500. Refactor,Comment: sitio/views.py

--- FCMusic/sitio/views.py
+++ FCMusic/sitio/views.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound, HttpResponseNotAllowed
 
 from .models import Cancion,Lista_reproduccion,Cancion_lista_reproduccion
 import json
 from django.core import serializers
 
 # Create your views here.
+# Obtiene el body de la petición, y regresa un objeto a partir del json obtenido
 def parseRequest(req):
     body_unicode = req.body
     body = json.loads(body_unicode)
     return body
+
+# Obtiene las canciones de una lista a partir de una petición
+# Regresa la lista de canciones que se obtuvieron
+def getCancionesLista(request):
+    body = parseRequest(request)
+
+    user_id = request.user.id
+    lista_id = body['lista_id']
+    res = Cancion_lista_reproduccion.objects \
+        .filter(Lista_reproduccion=lista_id) \
+        .selectRelated('Cancion')
+
+    return list(res)
+
+# Obtiene las canciones a partir de su nombre
+# Regresa una respuesta en json con la lista de las canciones encontradas
+def getCancion(request):
+    body = parseRequest(request)
+
+    titulo_req = body['titulo']
+    result = list(Cancion.objects.filter(titulo__icontains=titulo_req))
+
+    json_res = serializers.serialize('json', result)
+    return HttpResponse(json_res)
 
 
 def home(request):
@@ -23,27 +48,6 @@ def home(request):
 def escuchar(request):
     return render(request, 'cancion/escuchar.html')
 
-def getCancion(request):
-    body_unicode = request.body
-    body = json.loads(body_unicode)
-
-    titulo_req = body['titulo']
-    result = list(Cancion.objects.filter(titulo__icontains=titulo_req))
-
-    json_res = serializers.serialize('json', result)
-    return HttpResponse(json_res)
-
-def getCancionesLista(request):
-    body_unicode = request.body
-    body = json.loads(body_unicode)
-
-    user_id = request.user.id
-    lista_id = body['lista_id']
-    res = Cancion_lista_reproduccion.objects \
-        .filter(Lista_reproduccion=lista_id) \
-        .selectRelated('Cancion')
-
-    return list(res)
 
 def escucharLista(request):
     lista = getCancionesLista(request)
@@ -71,3 +75,19 @@ def putLista(request):
         i+=1
 
     return HttpResponseRedirect('/home')
+
+
+# Errores
+
+def error404(request, exception):
+    return HttpResponseNotFound()
+    # return render(request, 'no_encontrada.html')
+
+def error400(request, exception):
+    return render(request, 'errores/error400.html')
+
+def error403(request, exception):
+    return render(request, 'errores/error403.html')
+
+def error500(request):
+    return render(request, 'errores/error500.html')
